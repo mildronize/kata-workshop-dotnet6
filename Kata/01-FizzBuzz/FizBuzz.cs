d5e3d27c3e515548f3d885c2274d738f6ae0c5da
Thada Wangthammang <thada.wth@gmail.com>
2022-03-19 14:22:43 +0700

    02-fizzbuzz: green with reorder condition

--- Kata/01-FizzBuzz/FizBuzz.cs
+++ Kata/01-FizzBuzz/FizBuzz.cs
@@ -3,12 +3,12 @@ namespace Kata.FizzBuzz;
 public class FizzBuzz
 {
     public string GetFizzBuzz(int number){
+        if(number % 3 == 0 && number % 5 == 0)
+            return "FizzBuzz";
         if(number % 3 == 0)
             return "Fizz";
         if(number % 5 == 0)
             return "Buzz";
-        if(number % 3 == 0 && number % 5 == 0)
-            return "FizzBuzz";
         return number.ToString(CultureInfo.InvariantCulture);
     }
 }
