adcba6c4841ec55c15508b5a9c31193bab8f02f2
Thada Wangthammang <thada.wth@gmail.com>
2022-03-19 14:19:49 +0700

    02-fizzbuzz: add fail case (15=>FizzBuzz)

--- Kata.Tests/01-FizzBuzz/FizzBuzzTests.cs
+++ Kata.Tests/01-FizzBuzz/FizzBuzzTests.cs
@@ -3,13 +3,20 @@ namespace Kata.FizzBuzz.Tests;
 public class FizzBuzzTests
 {
     [Theory]
+    // 1 Case
     [InlineData(1, "1")]
+    // Fizz Case
     [InlineData(3, "Fizz")]
     [InlineData(6, "Fizz")]
 
+    // Buzz Case
     [InlineData(5, "Buzz")]
 
+    // Otherwise Case
     [InlineData(2, "2")]
+
+    // FizzBuzz Case
+    [InlineData(15, "FizzBuzz")]
     public void CanTestSingleNumber(int input, string expected)
     {
         // Arrange
