7b9ba2035393c06c5fef089b055ce7236f4a4360
Thada Wangthammang <thada.wth@gmail.com>
2022-03-19 14:12:36 +0700

    02-fizzbuzz: green with better solution if n%5 ==0 -> Buzz

--- Kata/01-FizzBuzz/FizBuzz.cs
+++ Kata/01-FizzBuzz/FizBuzz.cs
@@ -6,6 +6,8 @@ public class FizzBuzz
     public string GetFizzBuzz(int number){
         if(number % 3 == 0)
             return "Fizz";
+        if(number % 5 == 0)
+            return "Buzz";
         return "1";
     }
 }
