5dff8a87bdd5d19fe956b61d1f9f8ab145da226d
Thada Wangthammang <thada.wth@gmail.com>
2022-03-19 14:17:42 +0700

    02-fizzbuzz: green with better solution -> otherwise case should return that input

--- Kata/01-FizzBuzz/FizBuzz.cs
+++ Kata/01-FizzBuzz/FizBuzz.cs
@@ -2,12 +2,11 @@ namespace Kata.FizzBuzz;
 
 public class FizzBuzz
 {
-
     public string GetFizzBuzz(int number){
         if(number % 3 == 0)
             return "Fizz";
         if(number % 5 == 0)
             return "Buzz";
-        return "1";
+        return number.ToString(CultureInfo.InvariantCulture);
     }
 }
