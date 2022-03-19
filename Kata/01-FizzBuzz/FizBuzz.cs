6562612fc6093e6510f549cab33353d9c0996570
Thada Wangthammang <thada.wth@gmail.com>
2022-03-19 14:21:58 +0700

    02-fizzbuzz: add wrong solution (wrong order of condition)

--- Kata/01-FizzBuzz/FizBuzz.cs
+++ Kata/01-FizzBuzz/FizBuzz.cs
@@ -7,6 +7,8 @@ public class FizzBuzz
             return "Fizz";
         if(number % 5 == 0)
             return "Buzz";
+        if(number % 3 == 0 && number % 5 == 0)
+            return "FizzBuzz";
         return number.ToString(CultureInfo.InvariantCulture);
     }
 }
